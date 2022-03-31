55219cb4c088eeba979d6436bfa381fb05a61869
Davis <10787876@uvu.edu>
2022-03-30 22:27:05 -0600

    code journal

--- CodeJournal.js
+++ CodeJournal.js
@@ -1,10 +1,10 @@
-// Variables - containers that store values.
+// Variables - containers that store values
 
 var name; // a declared variable, but not initialized (no value) and it's in the "global scope" (BAD)
 
-let foo; // a declared ES 6 variable that can be changed - still no value however 
+let foo; // a declared ES 6 variable that can be changed - still no value however
 
-const bar = "Bar" // a declared ES 6 constant that cannot be changed.
+const bar = "Bar"; // a declared ES 6 constant that cannot be changed
 // '=' is the assignment operator, read it as "is assigned the value of..."
 
 const ANSWER = 42;
@@ -19,11 +19,11 @@ let string3 = new String("Hello New World!")  // using a String constructor
 
 // Numbers
 
-let myNum = 888111333;
+let myNum = 23908749287;
 
-let myNum2 = 25.94;
+let myNum2 = 75.25;
 
-"1" // This is not a number! It is a string.
+"1" // is not a number!  It is a string.
 // "==" a loose equality check
 "1" == 1; // evaluates to true because of type coercion and loose equality checking
 "1" === 1; // false because this is strict equality checking
@@ -38,12 +38,12 @@ let myBool = false;
 
 let myArray = [];  // this is an empty array - notice the square brackets
 
-// ordering     0      1      2       3      4
+// ordering     0     1      2       3      4
 let myArray2 = [42, "Bob", myBool, ANSWER, true];
 
-let secondElement = myArray2[1];  // retrieve the item at the 1 or second position of the array.
+let secondElement = myArray2[1];  // retreive the item at the 1 or second position of the array
 
-myArray2.length;  //  the length property of an array is very handy.
+myArray2.length;  // the length property of an array is very handy
 
 let lastItem = myArray2[myArray2.length - 1];
 
@@ -51,30 +51,86 @@ let lastItem = myArray2[myArray2.length - 1];
 
 let minObject = {}; // the most minimal object possible
 
-let myCar = { // objects are made of properties which are key:value pairs.
-    make: 'Chevrolet',
-    color: 'Green',
-    year: '1964',
-    vin: '88888888',
+let myCar = { // objects are made of properties which are key:value pairs
+  make: 'Chevrolet',
+  color: 'Green',
+  year: '1964',
+  vin: '29384732874329874'
 }
 
-myCar.numDoors = 4; // a new property can be simply added to an existing object using dot notation
+myCar.numDoors = 4;  // a new property can be simply added to an existing object using dot notation
 
-const anotherObject = { // objects can contain just about anything, including arrays, other objects, etc.
-    wordz: ['foo', 'bar', 'baz'],
-    car: {
-        make: 'Ferrari',
-        model: 'LaFerrari'
-    },
-    dope: true
+const anotherObject = {  // objects can contain just about anything, including arrays, other objects, etc.
+  wordz: ['foo', 'bar', 'baz'], 
+  car: {
+    make: 'McLaren',
+    model: '720s'
+  },
+  awesomeness: true
 };
 
 // Functions
 
-function myFunction() { // this is a named function definition 
-    return "My condolences to you is what I return!"; // doesn't do much. Only return a string.
+function myFunction() { // this is a named function definition
+  return "My greeting to you is what I return to you!"; // doesn't do much.  Only returns a string.
 }
 
 function sumTwoThings(thingOne, thingTwo) {
-    return thingOne + thingTwo;
+  return thingOne + thingTwo;
 }
+
+// Filter method example.  Filter returns an array of all elements that 'pass the test'
+const pilots = [
+  {
+    id: 2,
+    name: "Wedge Antilles",
+    faction: "Rebels"
+  },
+  {
+    id: 8,
+    name: "Ciena Ree",
+    faction: "Empire"
+  },
+  {
+    id: 40,
+    name: "Iden Versio",
+    faction: "Empire"
+  },
+  {
+    id: 66,
+    name: "Thane Kyrell",
+    faction: "Rebels"
+  }
+]
+
+const rebels = pilots.filter(pilot => pilot.faction === "Rebels")
+
+const empire = pilots.filter((pilot) => {
+  return pilot.faction === 'Empire'
+})
+
+// Array helper method 'map' example
+
+let filmURLs = [
+  "https://swapi.co/api/films/",
+  "https://swapi.co/api/films/5/",
+  "https://swapi.co/api/films/4/this one is longer... even longer",
+  "https://swapi.co/api/films/6/",
+  "https: ",
+  "https://swapi.co/api/films/1/"
+]
+
+const filmLengths = filmURLs.map(filmURL => filmURL.length)
+
+const filmPlusMore = filmURLs.map(filmURL => {
+  let filmObj = {
+    url: filmURL,
+    createdDate: Date.now()
+  }
+  return filmObj
+})
+
+const pilotNames = pilots.map(pilot => pilot.name) // new array just contains pilot's names
+
+
+// Ternary operator syntax: condition ? exprIfTrue : exprIfFalse
