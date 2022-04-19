8b4670554f297470bbd76ff207c759410449ae54
Davis <10787876@uvu.edu>
2022-04-19 11:19:27 -0600

    updated pokemon colors and gradient option

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -7,24 +7,36 @@ const getAPIData = async (url) => {
   }
 };
 
+const loadedPokemon = [];
+
 async function loadPokemon(offset = 0, limit = 25) {
   const pokeData = await getAPIData(
     `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
   );
   for (const nameAndUrl of pokeData.results) {
     const pokemon = await getAPIData(nameAndUrl.url);
-    populatePokeCard(pokemon);
+    const simplifiedPokemon = {
+      id: pokemon.id,
+      height: pokemon.height,
+      weight: pokemon.weight,
+      name: pokemon.name,
+      types: pokemon.types,
+      abilities: pokemon.abilities,
+      moves: pokemon.moves.slice(0, 3),
+    };
+    loadedPokemon.push(simplifiedPokemon);
+    populatePokeCard(simplifiedPokemon);
   }
 }
 
 class Pokemon {
   constructor(name, height, weight, abilities, types) {
-      this.id = 9001,
-      this.name = name,
-      this.height = height,
-      this.weight = weight,
-      this.abilities = abilities,
-      this.types = types
+    (this.id = 9001),
+      (this.name = name),
+      (this.height = height),
+      (this.weight = weight),
+      (this.abilities = abilities),
+      (this.types = types);
   }
 }
 
@@ -33,38 +45,41 @@ newButton.textContent = "New Pokemon";
 const header = document.querySelector("header");
 header.appendChild(newButton);
 newButton.addEventListener("click", () => {
-
-  const pokeName = prompt('What is the name of your new Pokemon?', 'Drizzydon')
-  const pokeHeight = prompt("What is the Pokemon's height?", 20)
-  const pokeWeight = prompt("What is the Pokemon's weight?", 1000)
-  const pokeAbilities = prompt("What are your Pokemon's abilities? (use a comma-separated list)")
-  const pokeTypes = prompt("What are your Pokemon's types? (up to 2 types separated by a space)")
+  const pokeName = prompt("What is the name of your new Pokemon?", "Drizzydon");
+  const pokeHeight = prompt("What is the Pokemon's height?", 20);
+  const pokeWeight = prompt("What is the Pokemon's weight?", 1000);
+  const pokeAbilities = prompt(
+    "What are your Pokemon's abilities? (use a comma-separated list)"
+  );
+  const pokeTypes = prompt(
+    "What are your Pokemon's types? (up to 2 types separated by a space)"
+  );
 
   const newPokemon = new Pokemon(
     pokeName,
     pokeHeight,
     pokeWeight,
     makeAbilitiesArray(pokeAbilities),
-    makeTypesArray(pokeTypes),
-  )
-  console.log(newPokemon)
-  populatePokeCard(newPokemon)
+    makeTypesArray(pokeTypes)
+  );
+  console.log(newPokemon);
+  populatePokeCard(newPokemon);
 });
 
 function makeAbilitiesArray(commaString) {
-  return commaString.split(',').map((abilityName) => {
+  return commaString.split(",").map((abilityName) => {
     return {
-      ability: { name: abilityName}
-    }
-  })
+      ability: { name: abilityName },
+    };
+  });
 }
 
 function makeTypesArray(spacedString) {
-  return spacedString.split(' ').map((typeName) => {
+  return spacedString.split(" ").map((typeName) => {
     return {
-      type: { name: typeName}
-    }
-  })
+      type: { name: typeName },
+    };
+  });
 }
 
 const pokeGrid = document.querySelector(".pokeGrid");
@@ -87,10 +102,19 @@ function populatePokeCard(pokemon) {
 function populateCardFront(pokemon) {
   const pokeFront = document.createElement("figure");
   pokeFront.className = "cardFace front";
-  const pokeImg = document.createElement("img");
+
+  const pokeType1 = pokemon.types[0].type.name
+  //const pokeType2 = pokemon.types[1]?.type.name
+  // console.log(pokeType1, pokeType2)
+  // console.log(getPokeTypeColor(pokeType1))
+  pokeFront.style.setProperty('background', getPokeTypeColor(pokeType1))
+/*   if(pokeType2) {
+    pokeFront.style.setProperty('background', `linear-gradient(${getPokeTypeColor(pokeType1)}, {$getPokeTypeColor(pokeType2)})`)
+  } */
+  const pokeImg = document.createElement('img')
   if (pokemon.id > 9000) {
     // load local image
-    pokeImg.src = '../images/pokeball.png'
+    pokeImg.src = "../images/pokeball.png";
   } else {
     pokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
   }
@@ -118,4 +142,51 @@ function populateCardBack(pokemon) {
   return pokeBack;
 }
 
-loadPokemon(0, 25);
+function getPokeTypeColor(pokeType) {
+  let color;
+  //if(pokeType === "grass") color = '#00FF00'
+  switch (pokeType) {
+    case "grass":
+      color = "#00FF00";
+      break;
+    case "fire":
+      color = "#FF0000";
+      break;
+    case "water":
+      color = "#0000FF";
+      break;
+    case "bug":
+      color = "#7FFF00";
+      break;
+    case "normal":
+      color = "#F5F5DC";
+      break;
+    case "flying":
+      color = "#00FFFF";
+      break;
+    case "poison":
+      color = "#C300FF";
+      break;
+    case "electric":
+      color = "#C8FF00";
+      break;
+    case "psychic":
+      color = "pink";
+      break;
+    case "ground":
+      color = "brown";
+      break;
+    default:
+      color = "#888888";
+  }
+  return color;
+}
+
+await loadPokemon(0, 250);
+
+function getPokemonByType(type) {
+  return loadedPokemon.filter((pokemon) => pokemon.types[0].type.name === type);
+}
+// now figure out how to display this count in the UI
+
+console.log(getPokemonByType("poison"));
