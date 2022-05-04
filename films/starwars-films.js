a69063117fdd092426ad5ab2eedbbd601e949001
Davis <10787876@uvu.edu>
2022-05-03 23:13:34 -0600

    styled and added release date to films

--- films/starwars-films.js
+++ films/starwars-films.js
@@ -3,13 +3,13 @@ import { getLastNumber } from '../utils/index.js'
 
 // Third, use a variable to store a 'reference' to the main element with an id attribute of 'filmList'
 let filmList = document.querySelector('#filmList')
-
 for (let i = 0; i < films.length; i++) {
   // First, create an img element
 
   let figure = document.createElement('figure')
   let figImage = document.createElement('img')
   let figCaption = document.createElement('figcaption')
+  let movieCaption = document.createElement('p')
 
   // Second, set the new image's source property to a valid URL or path
   let filmNum = getLastNumber(films[i].url)
@@ -17,10 +17,10 @@ for (let i = 0; i < films.length; i++) {
   figImage.src = `https://starwars-visualguide.com/assets/img/films/${filmNum}.jpg`
 
   figCaption.textContent = films[i].title
-
+  movieCaption.textContent = `Released ${films[i].release_date}`
   // Fourth, append the newly created img element as a child of the main element to make it appear in the DOM
   figure.appendChild(figImage)
   figure.appendChild(figCaption)
-
   filmList.appendChild(figure)
+  figCaption.appendChild(movieCaption)
 }
